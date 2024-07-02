0690807a69e92fcec97c73abdb09c6f3a4bcc780
Fripok <azceus@gmail.com>
2024-07-02 17:06:40 +0200

    originvalues deleted

--- scripts/values.js
+++ scripts/values.js
@@ -58,62 +58,62 @@ function extractValuesFromHTML(html) {
 	return extractedValues;
 }
 
-function getOriginValues() {
-	if(last_update)
-	{
-		document.getElementById('last-update').innerHTML = last_update;
-	}
-	const fullscreenBtn = document.getElementById('refreshBtn');
-	fullscreenBtn.style.boxShadow = "inset 2px 2px 0px 0px #905700, inset 0px 2px 0px 0px #e0aa00, inset 0px 0px 0px 2px #582400, inset 0px -2px 0px 0px #582400, inset 0px 0px 0px 0px #905700";
-	fetch('https://originvalues.com/')
-		.then(response => response.text())
-		.then(html => {
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(html, 'text/html');
-			const xpath1 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[1]';
-			const xpath2 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[2]';
-			const xpath3 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/center';
-			function extractContentByXPath(doc, xpath) {
-				if (doc instanceof Document && doc.evaluate) {
-					const result = doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-					return result.singleNodeValue ? result.singleNodeValue.innerHTML : 'Contenu non trouvé';
-				} else {
-					console.error("L'objet 'doc' n'est pas un document valide.");
-					return 'Contenu non trouvé';
-				}
-			}
-			let rares = extractContentByXPath(doc, xpath1);
-			let club = extractContentByXPath(doc, xpath2);
-			let updateText = extractContentByXPath(doc, xpath3);
-			const dateRegex = /Updated: (\w+) (\d+)th, (\d+) at (\d+:\d+PM EST)/;
-			const matches = updateText.match(dateRegex);
-			if (matches) {
-				const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-				const month = matches[1];
-				const day = matches[2];
-				const year = matches[3];
-				const time = matches[4];
-				const monthNumber = (monthNames.indexOf(month) + 1).toString().padStart(2, '0');
-				const formattedDay = day.padStart(2, '0');
-				last_update = `${monthNumber}/${formattedDay}/${year} ${time}`;
-				document.getElementById('last-update').innerHTML = last_update;
-			} else {
-				console.log("La date n'a pas pu être extraite.");
-			}
-			rares = extractValuesFromHTML(rares);
-			club = extractValuesFromHTML(club);
-			document.getElementById('webtrader').href="https://originvalues.com/";
-			document.getElementById('webtrader').text="Visit:originvalues.com";
-			setTimeout(() => {
-				document.getElementById('loader').style.display = 'none';
-				appendItems(rares)
-				appendItems(club)
-				fullscreenBtn.style.boxShadow = "";
-			}, 1000);
+// function getOriginValues() {
+// 	if(last_update)
+// 	{
+// 		document.getElementById('last-update').innerHTML = last_update;
+// 	}
+// 	const fullscreenBtn = document.getElementById('refreshBtn');
+// 	fullscreenBtn.style.boxShadow = "inset 2px 2px 0px 0px #905700, inset 0px 2px 0px 0px #e0aa00, inset 0px 0px 0px 2px #582400, inset 0px -2px 0px 0px #582400, inset 0px 0px 0px 0px #905700";
+// 	fetch('https://originvalues.com/index.html')
+// 		.then(response => response.text())
+// 		.then(html => {
+// 			const parser = new DOMParser();
+// 			const doc = parser.parseFromString(html, 'text/html');
+// 			const xpath1 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[1]';
+// 			const xpath2 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/div[2]';
+// 			const xpath3 = '/html/body/table/tbody/tr/td/table[3]/tbody/tr/td/table[2]/tbody/tr/td[3]/table/tbody/tr/td/center';
+// 			function extractContentByXPath(doc, xpath) {
+// 				if (doc instanceof Document && doc.evaluate) {
+// 					const result = doc.evaluate(xpath, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+// 					return result.singleNodeValue ? result.singleNodeValue.innerHTML : 'Contenu non trouvé';
+// 				} else {
+// 					console.error("L'objet 'doc' n'est pas un document valide.");
+// 					return 'Contenu non trouvé';
+// 				}
+// 			}
+// 			let rares = extractContentByXPath(doc, xpath1);
+// 			let club = extractContentByXPath(doc, xpath2);
+// 			let updateText = extractContentByXPath(doc, xpath3);
+// 			const dateRegex = /Updated: (\w+) (\d+)th, (\d+) at (\d+:\d+PM EST)/;
+// 			const matches = updateText.match(dateRegex);
+// 			if (matches) {
+// 				const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// 				const month = matches[1];
+// 				const day = matches[2];
+// 				const year = matches[3];
+// 				const time = matches[4];
+// 				const monthNumber = (monthNames.indexOf(month) + 1).toString().padStart(2, '0');
+// 				const formattedDay = day.padStart(2, '0');
+// 				last_update = `${monthNumber}/${formattedDay}/${year} ${time}`;
+// 				document.getElementById('last-update').innerHTML = last_update;
+// 			} else {
+// 				console.log("La date n'a pas pu être extraite.");
+// 			}
+// 			rares = extractValuesFromHTML(rares);
+// 			club = extractValuesFromHTML(club);
+// 			document.getElementById('webtrader').href="https://originvalues.com/";
+// 			document.getElementById('webtrader').text="Visit:originvalues.com";
+// 			setTimeout(() => {
+// 				document.getElementById('loader').style.display = 'none';
+// 				appendItems(rares)
+// 				appendItems(club)
+// 				fullscreenBtn.style.boxShadow = "";
+// 			}, 1000);
 			
-		})
-		.catch(error => console.error('Erreur lors de la récupération des données:', error));
-}
+// 		})
+// 		.catch(error => console.error('Erreur lors de la récupération des données:', error));
+// }
 
 function appendItems(items) {
 	const container = document.getElementById('items');
@@ -128,13 +128,13 @@ function appendItems(items) {
 	  <div class="dialog left">
 		<div class="row">
 		  <div class="furni_icon">
-			<img class="furni_icon" src="https://originvalues.com/imgs/furni/habbo_cola.png"/>
+			<img class="furni_icon" src="https://static.originvalues.com/imgs/furni/habbo_cola.png"/>
 		  </div>
 		  <div class="warning__title">${item.colaValue}</div>
 		</div>
 		<div class="row">
 		  <div class="furni_icon">
-			<img class="furni_icon" src="https://originvalues.com/imgs/hc_icon.png"/>
+			<img class="furni_icon" src="https://static.originvalues.com/imgs/hc_icon.png"/>
 		  </div>
 		  <div class="warning__title">${item.hcValue}</div>
 		</div>
@@ -157,12 +157,13 @@ function getTraderClubValues()
     .then(data => {
 		let item = data.find(item => item.name === "Cola Machine");
 		let hcval = item ? parseFloat(item.hc_val) : null;
+		let updated_at = null;
 		data.forEach(item => {
 			let name = item.name;
 			let colaValue = convertHcToCola(item.hc_val, hcval);
 			let hcValue = item.hc_val;
 			let image = item.image;
-			console.log(name, colaValue, hcValue, image)
+			updated_at = item.updated_at;
 			extractedValues.push({
 				name,
 				colaValue,
@@ -172,10 +173,10 @@ function getTraderClubValues()
 		});
 		document.getElementById('webtrader').href="https://traderclub.gg/";
 		document.getElementById('webtrader').text="Visit:traderclub.gg";
-		document.getElementById('last-update').innerHTML = "Soon";
 		setTimeout(() => {
 			document.getElementById('loader').style.display = 'none';
 			appendItems(extractedValues)
+			document.getElementById('last-update').innerHTML = updated_at;
 			fullscreenBtn.style.boxShadow = "";
 		}, 1000);
     })
@@ -186,10 +187,11 @@ function getTradeValues(site)
 {
 	document.getElementById('loader').style.display = 'block';
 	document.getElementById('items').innerHTML = '<div id="last-update" class="time"></div>';
-	if(site === "originvalues")
-	{
-		getOriginValues();
-	}else if(site === "traderclub")
+	// if(site === "originvalues")
+	// {
+	// 	getOriginValues();
+	// }
+	if(site === "traderclub")
 	{
 		getTraderClubValues();
 	}
